7d717659ef4758afcb1bb5302e32b6f0b28dd42f
EdwinVan <2576420317@qq.com>
2020-10-01 23:59:28 +0800

    Python函数部分学习

---
+++ 10-1/function.py
@@ -0,0 +1,11 @@
+# 测试函数关键字参数
+# fyj
+
+# 定义print_str函数
+def print_str(Str,num):
+    print(num)
+    print(Str)
+    return
+
+# 调用print_str函数
+print_str(num=1 , Str="Hello,Python!")
---
+++ 10-1/function2.py
@@ -0,0 +1,9 @@
+# 不定长参数
+# fyj
+
+def print_str2(a,*b):
+    print(a)
+    print(b)
+    return
+
+print_str2(1,2,3,4)
