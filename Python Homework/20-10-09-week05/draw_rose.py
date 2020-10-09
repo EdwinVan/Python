abf9933d74819a48033f487275aa9907eefd7517
EdwinVan <2576420317@qq.com>
2020-10-09 22:54:02 +0800

    Class

--- Python Homework/20-10-09-week05/draw_rose.py
+++ Python Homework/20-10-09-week05/draw_rose.py
@@ -11,10 +11,11 @@ turtle.fd(300)
 turtle.seth(0)
 turtle.pendown()
 
-turtle.pensize(2)
+turtle.pensize(6)
 
 # 花蕊
-
+turtle.fillcolor("red")
+turtle.begin_fill()
 turtle.circle(30,90)
 turtle.left(40)
 turtle.circle(40,170)
@@ -34,25 +35,28 @@ turtle.fd(50)
 
 
 # 左边花瓣
-
+turtle.pensize(4)
 turtle.seth(95)
 turtle.fd(20)
 turtle.seth(-100)
-turtle.circle(170,60)
+turtle.circle(170,60)  # 左边花瓣左边部分
+
 turtle.seth(45)
-turtle.circle(90,80)
+turtle.circle(90,80)   # 左边花瓣右边部分
+
 turtle.seth(-180)
-turtle.circle(-90,48)
+turtle.circle(-90,48)  
 
 turtle.left(180)
-turtle.circle(90,48)
+turtle.circle(90,48)   
+
 turtle.seth(-135)
-turtle.circle(90,80)
+turtle.circle(90,80)   
 
 
 # 右边花瓣
 turtle.left(180)
-turtle.circle(-90,80)
+turtle.circle(-90,80)   
 turtle.seth(0)
 
 for i in range(9):
@@ -61,9 +65,41 @@ for i in range(9):
 for i in range(10):
     turtle.fd(6)
     turtle.left(8)
+turtle.end_fill()
+
+turtle.fillcolor("red")
+turtle.begin_fill()
 
 turtle.left(180)
-turtle.circle(-139,100)
+turtle.circle(-139,100) # 右边花瓣右边部分
+
+turtle.seth(45)
+turtle.circle(90,80)   # 左边花瓣右边部分
+
+turtle.seth(-180)
+turtle.circle(-90,48)  
+
+turtle.left(180)
+turtle.circle(90,48)   
+
+turtle.seth(-135)
+turtle.circle(90,80) 
+
+turtle.left(180)
+turtle.circle(-90,80)   
+turtle.seth(0)
+
+for i in range(9):
+    turtle.fd(9)
+    turtle.left(3)
+for i in range(10):
+    turtle.fd(6)
+    turtle.left(8)
+turtle.end_fill()
+
+turtle.left(180)
+turtle.circle(-139,100)   # 右边花瓣右边部分
+
 
 # 花枝
 turtle.seth(-90)
@@ -77,7 +113,8 @@ for i in range(10):
 
 # 第1片叶子（上）
 turtle.seth(20)
-
+turtle.fillcolor("green")
+turtle.begin_fill()   
 for i in range(10):
     turtle.fd(10)
     turtle.right(1)
@@ -93,7 +130,7 @@ turtle.seth(50)
 turtle.circle(-90,90)
 turtle.left(-90)
 turtle.circle(-90,90)
-
+turtle.end_fill()
 
 
 turtle.left(60)
@@ -105,7 +142,8 @@ for i in range(5):
 
 # 第2片叶子（下）
 turtle.seth(175)
-
+turtle.fillcolor("green")
+turtle.begin_fill()
 for i in range(12):
     turtle.fd(10)
     turtle.right(1)
@@ -121,7 +159,7 @@ turtle.seth(130)
 turtle.circle(90,90)
 turtle.left(90)
 turtle.circle(90,90)
-
+turtle.end_fill()
 
 turtle.left(-47)
 turtle.fd(50)
